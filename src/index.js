8200239e636461ecfc1ff943f785cb7fdd4f0f79
prashanth.r <prashanth.ramadass@volvocars.com>
2022-06-20 07:56:53 +0200

    Prometheus metrics configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+const client = require('prom-client');
 const express = require('express');
 const app = express();
 const port = 3000;
+const register = new client.Registry();
+const collectDefaultMetrics = client.collectDefaultMetrics;
 
-app.get('/', (req, res) => {
+collectDefaultMetrics({ register });
+
+app.get('/demo', (req, res) => {
   res.send('GitHub Action To EKS Demo Working Fine');
+});
+
+app.get('/metrics', async (req, res) => {
+  var metrics = await register.metrics();
+  res.setHeader('Content-Type', register.contentType);
+  res.end(metrics);
 });
 
 app.listen(port, () => {
